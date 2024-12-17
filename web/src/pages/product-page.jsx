af13ad0f598e9adedcf8fe72b557f1dad08622f9
Ana Molina <ana713w@gmail.com>
2024-12-17 20:11:20 +0100

    feat: add cart from product page

--- web/src/pages/product-page.jsx
+++ web/src/pages/product-page.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import * as ProductsApi from "../services/products-services";
 import { useParams } from "react-router-dom";
 
-function ProductPage() {
+function ProductPage({ onAddCart }) {
   const { id } = useParams();
   const [product, setProduct] = useState(null);
 
@@ -16,18 +16,25 @@ function ProductPage() {
     return <div>Loading...</div>;
   }
 
+  const handleProductAddCart = (product) => {
+    ProductsApi.addCart(product.id)
+      .then(() => console.log("product add to cart"))
+      .catch((error) => console.log(error));
+  };
+
   return (
     <div className="card">
       <img src={product.image} className="card-img-top" alt="product image" />
       <div className="card-body">
         <h5 className="card-title">{product.name}</h5>
         <p className="card-text">${product.price}</p>
-        <button className="btn btn-primary">
+        <button
+          className="btn btn-primary"
+          onClick={() => handleProductAddCart(product)}
+        >
           Añadir al carrito
         </button>
-        <button className="btn btn-secondary">
-          Fav
-        </button>
+        <button className="btn btn-secondary">Fav</button>
       </div>
     </div>
   );
