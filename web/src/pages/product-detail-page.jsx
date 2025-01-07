8a9f60c11050c2bf2ed30383dab4bda7644d4a66
Ana Molina <ana713w@gmail.com>
2025-01-07 18:54:01 +0100

    product detail

--- web/src/pages/product-detail-page.jsx
+++ web/src/pages/product-detail-page.jsx
@@ -1,6 +1,8 @@
-import { useEffect, useState } from "react";
-import * as ProductsApi from "../services/products-services";
-import { useParams } from "react-router-dom";
+import { useState, useEffect } from 'react';
+import StarIcon from "@mui/icons-material/Star";
+import { Radio, RadioGroup } from '@headlessui/react';
+import { useParams } from 'react-router-dom';
+import * as ProductsApi from '../services/products-services';
 
 function ProductDetailPage({ onAddCart }) {
   const { id } = useParams();
@@ -18,30 +20,135 @@ function ProductDetailPage({ onAddCart }) {
 
   const handleProductAddCart = (product) => {
     ProductsApi.addCart(product.id)
-      .then(() => console.log("product add to cart"))
+      .then(() => console.log('Product added to cart'))
       .catch((error) => console.log(error));
   };
 
+  const classNames = (...classes) => {
+    return classes.filter(Boolean).join(' ');
+  };
+
+  const reviews = product.reviews || [];
+
   return (
-    <div className="bg-white rounded-lg shadow-md overflow-hidden">
-      <img
-        src={product.image}
-        className="w-full h-64 object-cover"
-        alt="product image"
-      />
-      <div className="p-4">
-        <h5 className="text-xl font-semibold text-gray-800">{product.name}</h5>
-        <p className="text-lg text-green-600 font-semibold">${product.price}</p>
-        <div className="mt-4 flex space-x-2">
-          <button
-            className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 transition duration-300"
-            onClick={() => handleProductAddCart(product)}
-          >
-            Añadir al carrito
-          </button>
-          <button className="bg-gray-300 text-gray-800 px-4 py-2 rounded-md hover:bg-gray-400 transition duration-300">
-            Fav
-          </button>
+    <div className="bg-white">
+      {/* Contenedor general de la Galería y la Información */}
+      <div className="mt-6 mx-auto px-6 lg:grid lg:grid-cols-3 lg:gap-x-8">
+
+        {/* Galería de imágenes */}
+        <div className="col-span-2 grid grid-cols-2 gap-4">
+          {/* Imagen principal */}
+          <img
+            alt={product.images[0].url}
+            src={product.images[0].url}
+            className="rounded-lg object-cover w-full h-full col-span-2 row-span-1"
+          />
+
+          {/* Otras imágenes */}
+          <div className="col-span-2 grid grid-cols-2 gap-4 mt-4">
+            {product.images.slice(1).map((image, index) => (
+              <img
+                key={index}
+                alt={image.url}
+                src={image.url}
+                className="rounded-lg object-cover w-full h-full col-span-2 row-span-1"
+              />
+            ))}
+          </div>
+        </div>
+
+        {/* Información del producto */}
+        <div className="sticky top-24 max-h-screen overflow-y-auto">
+          <h1 className="text-2xl font-bold text-gray-900 sm:text-3xl">{product.name}</h1>
+          <p className="text-3xl tracking-tight text-gray-900">${product.price}</p>
+
+          {/* Reseñas */}
+          <div className="mt-6">
+            <h3 className="sr-only">Reviews</h3>
+            <div className="flex items-center">
+              <div className="flex items-center">
+                {[0, 1, 2, 3, 4].map((rating) => (
+                  <StarIcon
+                    key={rating}
+                    aria-hidden="true"
+                    className={classNames(
+                      product.rating > rating ? 'text-gray-900' : 'text-gray-200',
+                      'size-5 shrink-0'
+                    )}
+                  />
+                ))}
+              </div>
+              <p className="sr-only">{product.rating} out of 5 stars</p>
+              <a href="#" className="ml-3 text-sm font-medium text-indigo-600 hover:text-indigo-500">
+                {reviews.length} reviews
+              </a>
+            </div>
+          </div>
+
+          {/* Opciones */}
+          <div className="mt-10">
+            {/* Colores */}
+            <div>
+              <h3 className="text-sm font-medium text-gray-900">Color</h3>
+              <fieldset aria-label="Choose a color" className="mt-4">
+                <RadioGroup value={product.swatches[0]} onChange={() => { }} className="flex items-center gap-x-3">
+                  {product.swatches.map((swatch) => (
+                    <Radio
+                      key={swatch.colorCode}
+                      value={swatch}
+                      aria-label={swatch.colorName}
+                      className="relative -m-0.5 flex cursor-pointer items-center justify-center rounded-full p-0.5 focus:outline-none"
+                    >
+                      <span
+                        aria-hidden="true"
+                        className={classNames('bg-' + swatch.colorCode, 'size-8 rounded-full border')}
+                      />
+                    </Radio>
+                  ))}
+                </RadioGroup>
+              </fieldset>
+            </div>
+
+            {/* Tallas */}
+            <div className="mt-10">
+              <h3 className="text-sm font-medium text-gray-900">Size</h3>
+              <fieldset aria-label="Choose a size" className="mt-4">
+                <RadioGroup className="grid grid-cols-4 gap-4 sm:grid-cols-8 lg:grid-cols-4">
+                  {product.available_sizes.map((size) => (
+                    <Radio key={size} value={size} className="cursor-pointer bg-white text-gray-900">
+                      <span>{size}</span>
+                    </Radio>
+                  ))}
+                </RadioGroup>
+              </fieldset>
+            </div>
+
+            {/* Añadir al carrito */}
+            <button
+              type="button"
+              onClick={() => handleProductAddCart(product)}
+              className="mt-10 flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-base font-medium text-white hover:bg-indigo-700"
+            >
+              Add to bag
+            </button>
+          </div>
+
+          {/* Descripción y detalles */}
+          <div className="py-10">
+            <h3 className="sr-only">Description</h3>
+            <p className="text-base text-gray-900">{product.description}</p>
+
+            <div className="mt-10">
+              <h3 className="text-sm font-medium text-gray-900">Materials</h3>
+              <ul role="list" className="list-disc space-y-2 pl-4 text-sm">
+                {product.materials.map((material, index) => (
+                  <li key={index} className="text-gray-400">
+                    <span className="text-gray-600">{material}</span>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
         </div>
       </div>
     </div>
--- web/src/pages/products-page.jsx
+++ web/src/pages/products-page.jsx
@@ -1,8 +1,9 @@
 import { useState } from "react";
 import FiltersSideBar from "../components/filters/filters-sidebar/filters-sidebar";
 import ProductList from "../components/products/product-list/product-list";
-import { Dialog, DialogTitle, DialogActions, DialogContent, Menu, MenuItem, IconButton, Button } from '@mui/material';
+import {   Dialog,  DialogBackdrop,  DialogPanel } from '@headlessui/react'
 import FilterAltIcon from '@mui/icons-material/FilterAlt';
+import CloseIcon from '@mui/icons-material/Close';
 
 function ProductsPage() {
   const [filters, setFilters] = useState({});
@@ -15,47 +16,59 @@ function ProductsPage() {
   return (
     <div className="bg-white">
       <div>
-      <Dialog
+        <Dialog
           open={FiltersOpen}
           onClose={() => setFiltersOpen(false)}
-          maxWidth="sm"
-          fullWidth
-          sx={{
-            position: 'absolute',
-            right: 0,
-            top: 0,
-            zIndex: 1300, 
-          }}
+          className="relative z-40"
         >
-          <DialogTitle>Filters</DialogTitle>
-          <DialogContent>
-            <FiltersSideBar onFilters={handleFilters} />
-          </DialogContent>
-          <DialogActions>
-            <Button onClick={() => setFiltersOpen(false)}>Close</Button>
-          </DialogActions>
-        </Dialog>
+          <DialogBackdrop
+            transition
+            className="fixed inset-0 bg-black/25 transition-opacity duration-300 ease-linear data-[closed]:opacity-0"
+          />
 
-        <main className="mx-auto">
-          <div className="flex sm:px-4 lg:px-7 items-baseline justify-between pt-6">
-            <h1 className="text-4xl font-bold font-montserrat tracking-tight text-gray-900">PRODUCTS</h1>
-
-            <div className="flex items-center">
-              <IconButton
-                className="ml-4 p-2 text-gray-400 hover:text-gray-500"
-                onClick={() => setFiltersOpen(true)}
-              >
-                <FilterAltIcon />
-              </IconButton>
-            </div>
+          <div className="fixed inset-0 z-40 flex">
+            <DialogPanel
+              transition
+              className="relative ml-auto flex size-full max-w-xs transform flex-col overflow-y-auto bg-white py-4 pb-12 shadow-xl transition duration-300 ease-in-out data-[closed]:translate-x-full"
+            >
+              <div className="flex items-center justify-between px-4">
+                <h2 className="text-lg font-medium text-gray-900">Filters</h2>
+                <button
+                  type="button"
+                  onClick={() => setFiltersOpen(false)}
+                  className="-mr-2 flex size-10 items-center justify-center rounded-md bg-white p-2 text-gray-400"
+                >
+                  <span className="sr-only">Close menu</span>
+                  <CloseIcon />
+                </button>
+              </div>
+              <FiltersSideBar onFilters={handleFilters} />
+            </DialogPanel>
           </div>
-
-          {/* Product List Section */}
-          <section className="pb-6 pt-6">
-            <ProductList filters={filters} />
-          </section>
-        </main>
+        </Dialog>
       </div>
+
+      <main className="mx-auto">
+        <div className="flex sm:px-4 lg:px-7 items-baseline justify-between pt-6">
+          <h1 className="text-4xl font-bold font-montserrat tracking-tight text-gray-900">PRODUCTS</h1>
+
+          <div className="flex items-center">
+            <button
+              type="button"
+              onClick={() => setFiltersOpen(true)}
+              className="ml-4 p-2 text-gray-400 hover:text-gray-500"
+            >
+              <span >Filters</span>
+              <FilterAltIcon />
+            </button>
+          </div>
+        </div>
+
+        {/* Product List Section */}
+        <section className="pb-6 pt-6">
+          <ProductList filters={filters} />
+        </section>
+      </main>
     </div>
   );
 }
