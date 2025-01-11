db39c83e802b0132f335f88f801bdaee120929c8
Ana Molina <ana713w@gmail.com>
2025-01-11 11:32:20 +0100

    fix/text whislist

--- web/src/pages/wishlist-page.jsx
+++ web/src/pages/wishlist-page.jsx
@@ -3,7 +3,7 @@ import WishList from "../components/wishlist/wish-list/wish-list";
 function WishlistPage() {
   return (
     <div className="p-5 font-montserrat">
-      <h3 className="text-4xl font-bold mb-4 text-dark-green font-montserrat">WishList</h3>
+      <h3 className="text-4xl font-bold mb-4 text-dark-green font-montserrat">WISHLIST</h3>
       <WishList />
     </div>
   );
